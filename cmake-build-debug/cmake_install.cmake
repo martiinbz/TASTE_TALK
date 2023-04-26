ed32907072f76af08584a0aceb95f892e97828ce
martiinbz <martin.barcena01@estudiant.upf.edu>
2023-04-26 09:02:53 +0200

    hola

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/marti/CLionProjects/EDA2
+# Install script for directory: C:/Users/marti/OneDrive/Escritorio/PROYECTO EDA 2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/marti/CLionProjects/EDA2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/marti/OneDrive/Escritorio/PROYECTO EDA 2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
