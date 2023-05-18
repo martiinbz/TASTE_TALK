351ea67e4604b519ce497411d1f3579ebe00a1c0
martiinbz <martin.barcena01@estudiant.upf.edu>
2023-05-18 19:37:16 +0200

    Changes by 34693

--- main.c
+++ main.c
@@ -2,7 +2,24 @@
 #include "estructuras.h"
 
 
-int main() {
-    printf("Hello, PAPAPAP!\n");
-    return 0;
+int main(){
+    Usuario *u;
+    printf("gola");
+    menu(*u);
+    printf("Main!");
+};
+
+int read_option(void){
+    int value;
+    if(scanf("%d", &value) != 1)
+        return -1;
+    return value;
+}
+
+void show_user_menu(void){
+    int option =-1;
+    while(option!=5)  //aqui supongo que la opcion 5 es salir
+        if(option == 1){
+
+        }
 }
