a370c028da684f41fc1d866549f4fb462ecab213
martiinbz <martin.barcena01@estudiant.upf.edu>
2023-05-18 19:40:47 +0200

    hola

---
+++ cmake-build-debug/CMakeFiles/EDA2.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/marti/OneDrive/Escritorio/EDA2/main.c" "CMakeFiles/EDA2.dir/main.c.obj" "gcc" "CMakeFiles/EDA2.dir/main.c.obj.d"
+  "C:/Users/marti/OneDrive/Escritorio/EDA2/menu principal.c" "CMakeFiles/EDA2.dir/menu_principal.c.obj" "gcc" "CMakeFiles/EDA2.dir/menu_principal.c.obj.d"
+  "C:/Users/marti/OneDrive/Escritorio/EDA2/pila.c" "CMakeFiles/EDA2.dir/pila.c.obj" "gcc" "CMakeFiles/EDA2.dir/pila.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
