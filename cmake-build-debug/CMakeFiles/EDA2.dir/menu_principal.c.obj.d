1e15b5699a351003798f9c2852bd6ce2f98f19ef
marti <martin.barcena01@estudiant.upf.edu>
2023-05-20 16:44:33 +0200

    k

---
+++ cmake-build-debug/CMakeFiles/EDA2.dir/menu_principal.c.obj.d
@@ -0,0 +1,13 @@
+CMakeFiles/EDA2.dir/menu_principal.c.obj: \
+ C:\Users\marti\CLionProjects\EDA2\menu\ principal.c \
+ E:/CLion\ 2022.3.1/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
+ E:/CLion\ 2022.3.1/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ E:/CLion\ 2022.3.1/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ E:/CLion\ 2022.3.1/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ E:/CLion\ 2022.3.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ E:/CLion\ 2022.3.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ E:/CLion\ 2022.3.1/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ E:/CLion\ 2022.3.1/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ E:/CLion\ 2022.3.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ E:/CLion\ 2022.3.1/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ E:/CLion\ 2022.3.1/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h
