6d200570edd3eafe617ce5dfe858c223603ee35d
marti <martin.barcena01@estudiant.upf.edu>
2023-06-03 13:45:34 +0200

    bug to raro

--- menu principal.c
+++ menu principal.c
@@ -25,7 +25,7 @@ void recopilar_datos( Usuario u[],int numero_de_usuarios){ //con esto se pregunt
 
     printf("\nIntroduce tu email");
     getchar();
-    gets(u[numero_de_usuarios].email);
+    fgets(u[numero_de_usuarios].email,20,stdin);
 
     printf("\nDe donde eres?");
     fgets(u[numero_de_usuarios].ubicacion,20,stdin);
@@ -38,24 +38,28 @@ void recopilar_datos( Usuario u[],int numero_de_usuarios){ //con esto se pregunt
     //esto es para imprimir los datos recolectados, por si hay algun error y necesitamos mirar si los recolecta bien
 };
 
-int recopilar_datos_fichero(Usuario u[],int numero_de_usuarios,FILE* fichero){
-   while (fscanf(fichero,"%s[^,] %s[^,] %d[^,] %s[^,] %s[^,] %s[^,] %s",u[numero_de_usuarios].nombre,u[numero_de_usuarios].usuario,&u[numero_de_usuarios].edad,u[numero_de_usuarios].contrasena,u[numero_de_usuarios].email,u[numero_de_usuarios].ubicacion,u[numero_de_usuarios].platos_favoritos)==7){
-
-       numero_de_usuarios+=1;
+/*int recopilar_datos_fichero(Usuario u[],int numero_de_usuarios,FILE* fichero,char *lista_de_usuarios[]){
+   while(fscanf(fichero,"%s %s %d %s %s %s %s",  u[numero_de_usuarios].nombre,u[numero_de_usuarios].usuario, &u[numero_de_usuarios].edad, u[numero_de_usuarios].contrasena, u[numero_de_usuarios].email, u[numero_de_usuarios].ubicacion, u[numero_de_usuarios].platos_favoritos)>0){
+       lista_de_usuarios[numero_de_usuarios]=u[numero_de_usuarios].usuario; //metemos el nombre de usuario en la lista
+       numero_de_usuarios += 1; //incrementamos el contador de usuarios.txt
+       lista_de_usuarios=(char**)realloc(lista_de_usuarios,sizeof(char*)*(numero_de_usuarios+1));//añadimos más memoria en caso de que se tenga que agregar un nuevo user.
    }
    return numero_de_usuarios;
 }
+ */
 void menu_principal(Usuario u[]) {
-    FILE* fichero= fopen("ficheros/courses.txt","r");
-    if(fichero==NULL){
-        printf("Esta vacio");
+    FILE *fichero;
+    char filename[] = "courses.txt";
+    fichero = fopen(filename, "r");
+    if (fichero == NULL) {
+        printf("Error opening file %s\n", filename);
     }
     int opcion=0,numero_de_usuarios=0;
     char **lista_de_usuarios;
     lista_de_usuarios=(char**)malloc(sizeof(char*)*(numero_de_usuarios+1));//creamos la lista dinamica con capacidad inicial 1
 
     printf("\n Welcome to Taste Talk!\n");
-    while(opcion!=4){
+    while(opcion<6){
         printf("\n 1. Registarse");
         printf("\n 2. Registrar usuarios.txt desde un fichero");
         printf("\n 3. Lista de Usuarios registrados");
@@ -70,22 +74,50 @@ void menu_principal(Usuario u[]) {
 
         }
         if(opcion==2){
-            while (fscanf(fichero,"%s[^,] %s[^,] %d[^,] %s[^,] %s[^,] %s[^,] %s",u[numero_de_usuarios].nombre,u[numero_de_usuarios].usuario,&u[numero_de_usuarios].edad,u[numero_de_usuarios].contrasena,u[numero_de_usuarios].email,u[numero_de_usuarios].ubicacion,u[numero_de_usuarios].platos_favoritos)>0){
+            while(fscanf(fichero,"%s %s %d %s %s %s %s",  u[numero_de_usuarios].nombre,u[numero_de_usuarios].usuario, &u[numero_de_usuarios].edad, u[numero_de_usuarios].contrasena, u[numero_de_usuarios].email, u[numero_de_usuarios].ubicacion, u[numero_de_usuarios].platos_favoritos)>0){
                 lista_de_usuarios[numero_de_usuarios]=u[numero_de_usuarios].usuario; //metemos el nombre de usuario en la lista
-                numero_de_usuarios+=1;
-                lista_de_usuarios=(char**)realloc(lista_de_usuarios,sizeof(char*)*(numero_de_usuarios+1));
+                numero_de_usuarios += 1; //incrementamos el contador de usuarios.txt
+                lista_de_usuarios=(char**)realloc(lista_de_usuarios,sizeof(char*)*(numero_de_usuarios+1));//añadimos más memoria en caso de que se tenga que agregar un nuevo user.
             }
-
-
         }
         if(opcion==3){
-            printf("\nHay %d usuarios.txt registrados,son:\n",numero_de_usuarios);
+            printf("\nHay %d usuarios registrados,son:\n",numero_de_usuarios);
             for(int i=0;i<=(numero_de_usuarios-1);i++){
-                printf("%s",lista_de_usuarios[i]); //esto va imprimiendo la lista de ususarios
+                printf("%s\n",lista_de_usuarios[i]); //esto va imprimiendo la lista de ususarios
             }
         }
-        if(opcion==3){
-            printf("\nPROXIMAMENTE\n");
+        if(opcion==4){
+            if(numero_de_usuarios==0) { //si aun no hay usuarios registrados
+                printf("\nNo hay usuarios registrados");
+            }
+            else{
+                char usuario_buscado[20];
+                printf("\nIntroduce el nombre de usuario: ");//el usuario introduce el nombre de usuario a buscar
+                getchar();
+                fgets(usuario_buscado, 20, stdin);
+
+                int indice_usuario = busqueda_secuencial(lista_de_usuarios, numero_de_usuarios, usuario_buscado);
+                printf("%d",indice_usuario);
+
+                if (indice_usuario != -1) {
+                    printf("\nUsuario encontrado: %s", u[indice_usuario].usuario);
+                    char contra[20];
+                    printf("Introduce la contrasena");
+                    fgets(contra,20,stdin);
+                    if(iniciar_sesion(&u,contra,indice_usuario)!=1){
+                        printf("Contrasena incorrecta, introducela de nuevo");
+                        fgets(contra,20,stdin);
+                    }
+                    if((iniciar_sesion(&u,contra,indice_usuario)==1)){
+                        printf("Bienvenido,%s",&u[indice_usuario].usuario);
+                    }
+                    submenu(u,indice_usuario);
+                } else {
+                    printf("\nUsuario no encontrado.");
+                }
+            }
+
+        }
         }
         if(opcion==5){
             printf("\nGracias por usar nuestra aplicacion!\n Hecha por:\n Martin Barcena \n Arnau Gil \n Adria Casals");
@@ -95,4 +127,3 @@ void menu_principal(Usuario u[]) {
             printf("\nSelecciona una opcion valida");
         }
     }
-}
